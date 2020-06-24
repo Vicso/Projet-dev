dfff7d606f2f6b37f7dc3424ebf0bfd485f48c17
guillaumeb74 <guillaume.brut@viacesi.fr>
2020-06-24 14:16:00 +0200

    windows db test ok

--- Dev/ClientWindows/Form1.cs
+++ Dev/ClientWindows/Form1.cs
@@ -23,12 +23,38 @@ namespace ClientWindows
             string connetionString;
             SqlConnection cnn;
 
-            connetionString = @"Data Source=ACER-PREDATOR-G;Initial Catalog=master;User ID=root;Password=root";
+            connetionString = @"Data Source=192.168.8.150,1433\ACER-PREDATOR-G;Initial Catalog=master;User ID=root;Password=root";
 
             cnn = new SqlConnection(connetionString);
             cnn.Open();
 
             MessageBox.Show("Connection Open  !");
+
+            db_action(cnn);
+
+        }
+
+        private void db_action(SqlConnection cnn)
+        {
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "SELECT [username],[password] FROM [user]";
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+
+            dataReader.Close();
+            command.Dispose();
             cnn.Close();
         }
 
