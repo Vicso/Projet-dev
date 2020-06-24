a629314d0e60fb6f0d464091e55583ff852b3a62
guillaumeb74 <guillaume.brut@viacesi.fr>
2020-06-24 11:37:16 +0200

    client windows + connection db sql OK

---
+++ Dev/ClientWindows/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+
+            connetionString = @"Data Source=ACER-PREDATOR-G;Initial Catalog=master;User ID=root;Password=root";
+
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
+        }
+
+    }
+}
